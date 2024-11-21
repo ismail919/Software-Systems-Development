a820a87a2f93962da33edf6f0b84a389f875d2c2
ismail919 <I.khan8993@student.leedsbeckett.ac.uk>
2024-11-21 11:53:55 +0000

    FINISHHH!!!!

---
+++ Lab7/Census.java
@@ -0,0 +1,45 @@
+package SSD.Lab7;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Census {
+    private List<Person> people = new ArrayList<Person>();
+
+    void addPerson(Person person) {
+        people.add(person);
+
+        boolean removePerson; (Person person)
+        return people.remove(person)
+
+        int countPeople () {
+            return people.size()
+        }
+        int countMales () {
+            int count = 0;
+            for (Person person : people) {
+
+                if (person.isMale())
+                    count++
+            }
+            return count;
+        )
+
+            int countFemales () {
+                return;
+                countPeople() - countFemales
+            }
+
+            void sortOnAge(){
+                Collections.sort()
+
+            }
+
+
+
+        }
+
+    }
+
+}
---
+++ Lab7/Person.java
@@ -0,0 +1,34 @@
+package SSD.Lab7;
+
+public class Person {
+
+    private int height;
+
+    private int age;
+
+    private boolean isMale;
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height){
+        this.height=height;
+    }
+
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public boolean isMale() {
+        return isMale;
+
+    }
+
+
+}
+
